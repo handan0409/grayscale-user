0d9590ac73d1d8cb1a4352b3e7720b73c925555f
韩丹 <handan05@58ganji.com>
2019-06-14 16:43:42 +0800

    updata

--- index.js
+++ index.js
@@ -3,13 +3,28 @@
  * handan
  * 做用户灰度策略
  */
-import cookie from "hd-cookie";
+let cookie = require("hd-cookie")  ;
+
+function grayscaleUser(scale) {
+  let userId = cookie.getCookieName('www58com', 'UserID');
+  if (!userId) {
+    userId = cookie.getCookieName('dk_cookie', "uid");
+  }
+  console.log(userId);
+  // 20 0.2 2  26    12312345 
+  scale = parseInt(scale);
+  userId = parseInt(userId);
+  if (!scale || !userId) return false;
+
+  let useridTen = userId * 1 % 100;
+  let scaleTen = scale * 1 % 100;
+
+  if (useridTen < scaleTen) {
+    return true;
+  }
+  return false;
+}
 
 
- function grayscaleUser(){
-  
- }
 
- 
-
- module.export = grayscaleUser;
+module.exports = grayscaleUser;
