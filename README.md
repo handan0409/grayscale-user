0d1abd9c0868e645a433ccb62768dbf56e03ba62
handan05 <handan05@58.com>
2019-11-25 17:33:09 +0800

    updata

--- README.md
+++ README.md
@@ -13,6 +13,7 @@ yarn add grayscale-user
 参数一：【必填】灰度的比例，0-99的正整数或字符串，如： 26，表示灰度26%；  <br>
 参数二：【选填】获取cookie中userId的值，根据这个数字做灰度，<br>
 参数三：【选填】若获取出来的value是一个query的话，获取query的name值 默认 UserID 或 uid  <br><br>
+参数四： 【选填】userid白名单，支持array 或 string
 
 返回值： boolean值，true：在灰度范围内，false：表示不再灰度范围内
 ```js
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let cookie = require("hd-cookie")  ;
 
 let newCookie = new cookie() ;
 
-function grayscaleUser(scale, cookie_name, name) {
+function grayscaleUser(scale, cookie_name, name, userIds) {
   let userId = "" ;
   if(name){
     userId = newCookie.getCookieName(cookie_name || 'PPU', name || 'UID');
@@ -20,7 +20,10 @@ function grayscaleUser(scale, cookie_name, name) {
       userId = newCookie.getCookie( cookie_name || 'dk_cookie');
     }
   }
-  
+  if(userIds && userIds.indexOf &&userIds.indexOf(userId) * 1 > -1){
+    // 在白名单内，不需要灰度
+    return true ;
+  }
   // console.log(userId);
   // 20 2  26    12312345 
   scale = parseInt(scale);
